d64dffe0946c388c539f85b7eeee0f31659723a4
Hakim Cassimally <hakim@mysociety.org>
2014-03-17 16:46:51 +0000

    Initial cobrand.
    This involves the initial work, rebranding, various fixes up to October 2014,
    and the addition of emergency services question in March 2015.
    
    And then all the additional changes to work with 2018 FixMyStreet master code,
    as it had been written now (so using hooks etc written in the past four years).

--- README.md
+++ README.md
@@ -1,15 +1,11 @@
 Collideoscope
 =============
 
-This repository holds extra collideoscope-specific code that doesn't belong in
+This repository holds Collideoscope-specific code that doesn't belong in
 the main [FixMyStreet repository](https://github.com/mysociety/fixmystreet).
 
-Currently, this is just the Perl code for processing Stats19 data into a
-FixMyStreet database.
-
-The main code that makes http://collideosco.pe work is still inside the
-[FixMyStreet repository](https://github.com/mysociety/fixmystreet) as a
-cobrand.
+This includes the Perl code for processing Stats19 data into a FixMyStreet
+database, plus all the cobrand code specific to Collideoscope.
 
 Usage
 -----
---
+++ web/cobrands/smidsy/js/report.js
@@ -0,0 +1,51 @@
+$(function() {
+
+    // Our validator plugin is old and breaks on date inputs with min/max
+    // Let's just disable its min/max validation entirely
+    delete $.validator.methods.min;
+    delete $.validator.methods.max;
+
+    $('input[name="severity"]').on('change', function(){
+        // Assumes the severity radio buttons have numeric values,
+        // where a value over 10 implies injury.
+        if( ($('#mapForm input[name="severity"]:checked').val() -0) > 10) {
+            $('.describe-injury').slideDown();
+        } else {
+            $('.describe-injury').slideUp(
+                // slideUp doesn't happen if element already hidden.
+                // But it does call callback so hide when complete.
+                // (We hide on callback, to avoid the hide killing the slide
+                // animation entirely.)
+                function () { $(this).hide() }
+            );
+        }
+    }).change(); // and call on page load
+
+    $('#form_participants').on('change', function(){
+        // In a stroke of genius, jQuery returns true for the :selected selector,
+        // if *any* of the matched elements are :selected, rather than *all* of them.
+        if( $('option[value="car"], option[value="motorcycle"], option[value="hgv"], option[value="other"]').is(':selected') ) {
+            $('.vehicle-registration-number').slideDown();
+        } else {
+            $('.vehicle-registration-number').slideUp();
+        }
+    }).change(); // and call on page load
+
+    var type = $('form.statistics-filter input[name=type]');
+    type.on('change', function () {
+        var val = $(this).val();
+        if (val == 'all') {
+            window.location = '/reports';
+        }
+        else if (val == 'london') {
+            window.location = '/reports?type=LBO';
+        }
+        else if (val == 'city') {
+            window.location = '/reports?type=UTA,MTD,COI';
+        }
+        else if (val == 'dc') {
+            window.location = '/reports?type=CTY,DIS';
+        }
+    });
+
+});
